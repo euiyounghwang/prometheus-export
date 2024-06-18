8d4643ac0a410ed2b206a19cc511adcf1bdeac9c
Euiyoung Hwang <euiyoung.hwang@gxo.com>
2024-06-18 15:06:19 -0500

    Update metrics

--- mail/mail_alert.py
+++ mail/mail_alert.py
@@ -1,10 +1,13 @@
 import subprocess
 import os
+import re
 
 # Function that send email.
 def send_mail(body, env, status, to):
     ''' send mail through mailx based on python environment'''
-    grafana_dashboard_url = os.environ["GRAFANA_DASHBORD_URL"]
+    if not body:
+        return
+    grafana_dashboard_url = os.environ["GRAFANA_DASHBOARD_URL"]
     body = "Monitoring [ES Team Dashboard on export application]'\n\t' \
         - Grafana Dashboard URL : {}'\n\t' \
         - Enviroment: {}'\n\t' \
@@ -38,8 +41,10 @@ def send_mail(body, env, status, to):
 
 if __name__ == '__main__':
     # Send Email
-    body_text = "all Kafka listeners are not active process running.."
-    
+    body_text = "org.apache.kafka.connect.errors.ConnectException: java.sql.SQLRecoverableException: IO Error: The Network Adapter could not establish the connection\n\tat io.confluent.connect.jdbc.util.CachedConnectionProvider.getValidConnection(CachedConnectionProvider.java:70)\n\tat io.confluent.connect.jdbc.source.JdbcSourceTask.poll(JdbcSourceTask.java:203)\n\tat org.apache.kafka.connect.runtime.WorkerSourceTask.execute(WorkerSourceTask.java:163)\n\tat org.apache.kafka.connect.runtime.WorkerTask.doRun(WorkerTask.java:146)\n\tat org.apache.kafka.connect.runtime.WorkerTask.run(WorkerTask.java:190)\n\tat java.util.concurrent.Executors$RunnableAdapter.call(Executors.java:511)\n\tat java.util.concurrent.FutureTask.run(FutureTask.java:266)\n\tat java.util.concurrent.ThreadPoolExecutor.runWorker(ThreadPoolExecutor.java:1149)\n\tat java.util.concurrent.ThreadPoolExecutor$Worker.run(ThreadPoolExecutor.java:624)\n\tat java.lang.Thread.run(Thread.java:748)\nCaused by: java.sql.SQLRecoverableException: IO Error: The Network Adapter could not establish the connection\n\tat oracle.jdbc.driver.T4CConnection.logon(T4CConnection.java:673)\n\tat oracle.jdbc.driver.PhysicalConnection.<init>(PhysicalConnection.java:715)\n\tat oracle.jdbc.driver.T4CConnection.<init>(T4CConnection.java:385)\n\tat "
+    body_text = body_text.replace('\n\t', " ").replace('\n', " ")
+    body_text = re.sub(r"[^\uAC00-\uD7A30-9a-zA-Z\s]", "", body_text)
+    print("str - ", body_text)
     email_list = os.environ["EMAIL_LIST"]
     print(f"mail_list : {email_list}")
     ''' Call function to send an email'''
--- standalone-export-run.sh
+++ standalone-export-run.sh
@@ -21,8 +21,7 @@ fi
 
 
 # -- Export Variable
-export GRAFANA_DASHBORD_URL="http://localhost:3000/d/adm08055cf3lsa/es-team-dashboard?orgId=1'&'from=now-5m'&'to=now'&'refresh=5s"
-export EMAIL_LIST="test@mail.com"
+export GRAFANA_DASHBOARD_URL="http://localhost:3000/d/adm08055cf3lsa/es-team-dashboard?orgId=1'&'from=now-5m'&'to=now'&'refresh=5s"
 # -- 
 
 # -- standalone type
